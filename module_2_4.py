2ebabaeefac1c3171b49d9ee91b4cc5cbafdf3d2
Novellina <nataliedenisova@gmail.com>
2024-11-20 22:23:10 +0500

    module_2_4.py

--- module_2_4.py
+++ module_2_4.py
@@ -1,20 +1,32 @@
-#numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
-#primes = []
-#not_primes = []
-#or i in range(1, 11):
-    #for j in range(1,11):
-        #print ('{i}X{j}={i*j}')
-import random
-def lottery(mon, tue):
-    tickets = [1,2,3,4,5,6,7,8,9,10]
-    win1 = random.choice(tickets)
-    tickets.remove(win1)
-    win2 = random.choice(tickets)
-    print (mon,tue)
-    return win1, win2
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+primes = []
+not_primes = []
+for i in numbers:
+    if i == 1:
+        continue
+    is_primes = True
+    for j in range(2,i):
+        if i % j == 0:
+            is_primes = False
+            break
+    if is_primes:
+        primes.append(i)
+    else:
+        not_primes.append(i)
+print(primes)
+print(not_primes)
 
-
-win1, win2 = lottery ('mon', 'tue')
-print(win1, win2)
+# import random
+# def lottery(mon, tue):
+#     tickets = [1,2,3,4,5,6,7,8,9,10]
+#     win1 = random.choice(tickets)
+#     tickets.remove(win1)
+#     win2 = random.choice(tickets)
+#     print (mon,tue)
+#     return win1, win2
+#
+#
+# win1, win2 = lottery ('mon', 'tue')
+# print(win1, win2)
 
 
